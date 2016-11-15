63854d40e11e82c6bd5f1a52ba1a9d27404e5fbe
Jay Kumar <jaykch@outlook.com>
2016-11-15 12:23:18 +0000

    Fixed cursors and selection of text

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 /**
  * Created by Jay on 11/14/2016.
  */
+$(document).ready(function(){
+    var subject = new selector("#x-selector");
+    subject.$id.click(function(){
+        console.log("clicked");
+    });
+});
+
+function log (){
+    console.log(arguments);
+}
+
+var selector = function (id){
+    this.$id = $(id);
+};
+
+
+
+
+
